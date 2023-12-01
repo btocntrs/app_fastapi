784cbefe712f46270589f2f9e1b2c43631e8c704
Alberto Contreras <btocntrs@gmail.com>
2023-12-01 16:25:50 -0600

    Se añadieron urls para prueba, dependiendo si se trabaja con AWS RDS, docker o la maquina local, respectivamente

--- app/database.py
+++ app/database.py
@@ -2,7 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://alberto:Alberto69.@db-testing.cmcatynqhv7p.us-east-1.rds.amazonaws.com/python_db"
+#DATABASE_URL = "postgresql://alberto:Alberto69.@db-testing.cmcatynqhv7p.us-east-1.rds.amazonaws.com/python_db"
+#DATABASE_URL = "postgresql://sinai:admin@postgreSQL/sinai_db"
+DATABASE_URL = "postgresql://sinai:admin@localhost/sinai_db"
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
