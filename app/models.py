7d1d4225bdabd1218af94387ddac7868fddb18a7
Alberto Contreras <btocntrs@gmail.com>
2023-12-05 15:15:46 -0600

    Se modificaron las string del modelo de la base de datos a strings o varchar de tamaño específico. Así será compatible tanto para postgres como para mysql

--- app/models.py
+++ app/models.py
@@ -7,10 +7,10 @@ class Proveedor(Base):
     __tablename__ = "proveedores"
     
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    rfc = Column(String, nullable=False, unique=True, index=True)
-    razon_social = Column(String, nullable=False, unique=True)
-    nombre_comercial = Column(String, unique=True)
-    productos = Column(String, nullable=False)
-    banco = Column(String)
-    cuenta = Column(String, unique=True)
-    telefono = Column(String, unique=True)
+    rfc = Column(String(13), nullable=False, unique=True, index=True)
+    razon_social = Column(String(100), nullable=False, unique=True)
+    nombre_comercial = Column(String(100), unique=True)
+    productos = Column(String(150), nullable=False)
+    banco = Column(String(80))
+    cuenta = Column(String(50), unique=True)
+    telefono = Column(String(20), unique=True)
