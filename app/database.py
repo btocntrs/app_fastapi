ed1944026beae39b9ef9cb9e5890524260251a53
Alberto Contreras <btocntrs@gmail.com>
2023-12-18 11:45:39 -0600

    Se eliminaron url inncesarias

--- app/database.py
+++ app/database.py
@@ -21,11 +21,7 @@ db_name = os.getenv("DB_NAME", default_db_name)
 
 # Construir la URL de la base de datos
 DATABASE_URL = f"{db_driver}://{user}:{password}@{host}:{port}/{db_name}"
-
 #DATABASE_URL = "postgresql://alberto:Alberto69.@db-testing.cmcatynqhv7p.us-east-1.rds.amazonaws.com/python_db"
-#DATABASE_URL = "postgresql://sinai:admin@postgreSQL/sinai_db"
-#DATABASE_URL = "postgresql://sinai:admin@localhost/sinai_db"
-
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
